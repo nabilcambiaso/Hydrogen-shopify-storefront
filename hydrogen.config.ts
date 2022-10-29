0dcb99e8afdb8a3bd34de7d49fee9ee65a44e5f1
nabilcambiaso <nabil.cambiaso@gmail.com>
2022-10-29 20:56:01 +0100

    Working hydrogen shopify storeFront

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'testforhydrogen.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
+      'adbf4184ef23706b17bf677a4cf586a4',
     privateStorefrontToken:
       // @ts-ignore
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
